42daac7462b9c4a457260abedf3610aa45dd0643
Ilja Koniev <IlyaKonka@users.noreply.github.com>
2022-09-18 19:35:16 +0200

    Update BotDefaults.java
    fix prop path

--- src/main/java/utils/BotDefaults.java
+++ src/main/java/utils/BotDefaults.java
@@ -4,6 +4,6 @@ public class BotDefaults {
     public static final long CHECK_TIMEOUT_MILLIS = 60000;
     public static final int BROWSER_TIMEOUT_SEC = 60;
     public static final int RESPONSE_TIMEOUT_SEC = 120;
-    public static final String PROPFILE = "src/main/resources/1global.properties";
+    public static final String PROPFILE = "src/main/resources/global.properties";
     public static final String FILENAME = "website.html";
 }
